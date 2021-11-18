498a42912cace0cd1bdddc16b0c0887f98e9358e
Wilson Lam <wlamcuhk@gmail.com>
2021-11-18 21:11:32 +0800

    updated

--- randomize/src/utils.py
+++ randomize/src/utils.py
@@ -5,22 +5,28 @@ from .randomize import OUTPUT_DIR, HIDDEN_DATA
 
 
 def hide_files():
+    
     for csv in os.listdir(OUTPUT_DIR):
                 if platform == 'darwin':
-                    if (csv.startswith(os.path.join(OUTPUT_DIR, HIDDEN_DATA[1:]))
+                    if (csv.startswith(HIDDEN_DATA[1:])
                         and csv.endswith('.csv')):
                             filename = csv.split(os.path.sep)[-1]
-                            os.rename(csv, os.path.join(OUTPUT_DIR,"."+filename))
+                            os.rename(os.path.join(OUTPUT_DIR, csv),
+                                      os.path.join(OUTPUT_DIR,"."+filename))
                 elif platform == 'win32':
-                    if csv.endswith('.csv'): os.popen('attrib +h ' + os.path.join(OUTPUT_DIR, csv))
+                    if csv.endswith('.csv'):
+                        os.popen('attrib +h ' + os.path.join(OUTPUT_DIR, csv))
+
 
 def unhide_files():
+
     for csv in os.listdir(OUTPUT_DIR):
             if platform == 'darwin':
-                    if (csv.startswith(os.path.join(OUTPUT_DIR, HIDDEN_DATA))
-                        and csv.endswith('.csv')):
+                    if (csv.startswith(HIDDEN_DATA) and csv.endswith('.csv')):
                             filename = csv.split(os.path.sep)[-1]
-                            os.rename(csv, os.path.join(OUTPUT_DIR, filename[1:]))
+                            os.rename(os.path.join(OUTPUT_DIR, csv),
+                                      os.path.join(OUTPUT_DIR, filename[1:]))
 
             elif platform == 'win32':
-                    if csv.endswith('.csv'): os.popen('attrib -h ' + os.path.join(OUTPUT_DIR, csv))
+                    if csv.endswith('.csv'):
+                        os.popen('attrib -h ' + os.path.join(OUTPUT_DIR, csv))
