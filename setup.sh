f660e53cf941c621fcce50ca64ad696ddab9eacd
Wilson Lam <wlamcuhk@gmail.com>
2022-10-04 12:06:34 +0800

    updated

--- setup.sh
+++ setup.sh
@@ -5,13 +5,27 @@
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Install Python3
-brew install python3
+# # Install Python3
+# brew install python3
+#
+# # Install Dependencies
+# pip3 install virtualenv virtualenvwrapper &&
+# python3 -m virtualenv venv &&
+# source venv/bin/activate &&
+# pip3 install -r requirements-core.txt &&
+# pip3 install -r requirements.txt
+# python3 -m setup build_ext --inplace -q
+
+# Install Anaconda
+brew install --cask anaconda
+PREFIX=/usr/local/anaconda3
+export PATH="/usr/local/anaconda3/bin:$PATH"
 
 # Install Dependencies
-pip3 install virtualenv virtualenvwrapper &&
-python3 -m virtualenv venv &&
-source venv/bin/activate &&
-pip3 install -r requirements-core.txt &&
-pip3 install -r requirements.txt
-python3 -m setup build_ext --inplace -q
+conda env create -f environment.yml
+
+# Activate environment
+conda activate pyrestus
+
+# Setup
+python3 -m setup build_ext --inplace -q 
